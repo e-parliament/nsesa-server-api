5e4f8cfcfd1e2dd71725901b893669f624bc8098
Philip Luppens <philip.luppens@gmail.com>
2013-05-02 16:39:18 +0200

    Moved additional method to the backend services.

--- src/main/java/org/nsesa/server/service/api/DocumentContentService.java
+++ src/main/java/org/nsesa/server/service/api/DocumentContentService.java
@@ -8,6 +8,7 @@ import javax.jws.WebService;
 import java.util.List;
 
 /**
+ * General service interface for {@link DocumentContentDTO}s.
  * Date: 11/03/13 15:38
  *
  * @author <a href="philip.luppens@gmail.com">Philip Luppens</a>
@@ -16,9 +17,36 @@ import java.util.List;
 @WebService
 public interface DocumentContentService {
 
+    /**
+     * Retrieve a single {@link DocumentContentDTO} via its <tt>documentID</tt>.
+     *
+     * @param documentID the public document ID
+     * @return the document content DTO, or <tt>null</tt> if it cannot be found
+     */
     DocumentContentDTO getDocumentContent(@WebParam(name = "documentID") String documentID);
 
+    /**
+     * Save the given <tt>documentContentDTO</tt> to the backend.
+     *
+     * @param documentContentDTO the document content DTO
+     */
     void saveDocumentContent(@WebParam(name = "documentContentDTO") DocumentContentDTO documentContentDTO);
 
+    /**
+     * List all the document content via paging.
+     *
+     * @param offset the paging offset
+     * @param rows   the number of document content objects to return
+     * @return the list of document contents
+     */
     List<DocumentContentDTO> list(@WebParam(name = "offset") int offset, @WebParam(name = "rows") int rows);
+
+    /**
+     * Get an XML fragment identified by <tt>xpathExpression</tt>.
+     *
+     * @param documentID      the document ID
+     * @param xpathExpression the xpath expression
+     * @return the fragment as XML string, or <tt>null</tt> if it cannot be found.
+     */
+    String getDocumentFragment(@WebParam(name = "documentID") String documentID, @WebParam(name = "xpathExpression") String xpathExpression);
 }
--- src/main/java/org/nsesa/server/service/api/DocumentService.java
+++ src/main/java/org/nsesa/server/service/api/DocumentService.java
@@ -8,6 +8,7 @@ import javax.jws.WebService;
 import java.util.List;
 
 /**
+ * General service for {@link DocumentDTO}s.
  * Date: 11/03/13 15:38
  *
  * @author <a href="philip.luppens@gmail.com">Philip Luppens</a>
@@ -15,9 +16,45 @@ import java.util.List;
  */
 @WebService
 public interface DocumentService {
+
+    /**
+     * Get a document via its <tt>documentID</tt>.
+     *
+     * @param documentID the public document ID
+     * @return the documentDTO, or <tt>null</tt> if it cannot be found.
+     */
     DocumentDTO getDocument(@WebParam(name = "documentID") String documentID);
 
-    void saveDocument(@WebParam(name = "documentDTO") DocumentDTO documentDTO);
+    /**
+     * Save a document DTO. This does normally not create a new revision.
+     *
+     * @param documentDTO the document DTO
+     * @return the new document DTO
+     */
+    DocumentDTO saveDocument(@WebParam(name = "documentDTO") DocumentDTO documentDTO);
 
+    /**
+     * Lists all documents in the backend. Paging available via <tt>offset</tt> and <tt>rows</tt>.
+     *
+     * @param offset the paging offset
+     * @param rows   the number of documents to return
+     * @return the list of documents
+     */
     List<DocumentDTO> list(@WebParam(name = "offset") int offset, @WebParam(name = "rows") int rows);
+
+    /**
+     * Returns all available translations for a given document identified by <tt>documentID</tt>.
+     *
+     * @param documentID the document ID
+     * @return the list of available translations
+     */
+    List<DocumentDTO> getAvailableTranslations(@WebParam(name = "documentID") String documentID);
+
+    /**
+     * Returns all available related documents for a given document identified by <tt>documentID</tt>.
+     *
+     * @param documentID the document ID
+     * @return the list of related documents
+     */
+    List<DocumentDTO> getRelatedDocuments(@WebParam(name = "documentID") String documentID);
 }
